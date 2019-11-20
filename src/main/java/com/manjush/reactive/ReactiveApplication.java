33dc8d15c2e6888cf322dfd43318b21abffee2ae
manjush <manjush.kochi@gmail.com>
2019-11-20 20:43:25 +0000

    Adding mongo db dependency

--- src/main/java/com/manjush/reactive/ReactiveApplication.java
+++ src/main/java/com/manjush/reactive/ReactiveApplication.java
@@ -2,10 +2,8 @@ package com.manjush.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class ReactiveApplication {
 
 	public static void main(String[] args) {
